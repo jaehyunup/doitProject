d61f2734ed7bfb64a2ab59538fd655b234540789
박재현 <pjh3909@gmail.com>
2019-08-17 22:52:13 +0900

    resource 설정

--- src/main/java/com/kakaolove/doit/HomeController.java
+++ src/main/java/com/kakaolove/doit/HomeController.java
@@ -23,7 +23,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	private TestService service;
 	
