5a17c5507dd14439b8d007cc49bf1dc877345d2b
PARKJAEHYUN <pjh3909@gmail.com>
2020-11-20 19:20:36 +0900

    Update ProfileController.java
    정크코드 삭제

--- src/main/java/com/kakaolove/doit/controller/ProfileController.java
+++ src/main/java/com/kakaolove/doit/controller/ProfileController.java
@@ -36,7 +36,6 @@ public class ProfileController {
     		try{
     			userprofilevo=(userProfileVO)profileservice.getuserProfile(authentication.getName());
     		}catch(Exception a){
-    			logger.info("프로필을 불러오지못하는 문제 발생");
     			return "redirect:/";
     		}
     		model.addAttribute("profile",userprofilevo);
@@ -48,17 +47,10 @@ public class ProfileController {
 	public String modifyProfile(userProfileVO userprofilevo,Principal principal,Locale locale,Model model) {    		
     		Authentication authentication=SecurityContextHolder.getContext().getAuthentication(); // context 인증정보 받기
     		userprofilevo.setId(authentication.getName());
-    		logger.info(userprofilevo.getId()); // x
-    		logger.info(userprofilevo.getIntroduce()); //o 
-    		logger.info(userprofilevo.getEdumajor()); // o
-    		logger.info(userprofilevo.getCertificate()); //o
-    		logger.info(userprofilevo.getNickname()); // o
-    		logger.info(userprofilevo.getPosition()); // o
-    		logger.info(userprofilevo.getSpec()); // o
     		userprofilevo.setFiled("Spring_Java");
     		
     		try{
-    			profileservice.modifyuserProfile(userprofilevo); // 리턴체크해야함
+    			profileservice.modifyuserProfile(userprofilevo); 
     			return "redirect:/";
     			
     		}catch(Exception a){
